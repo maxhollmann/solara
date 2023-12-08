eacafda4010e8d684fdcb24a558c64fab25cf149
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-12-08 10:25:36 +0100

    fix: clean up thread correctly to avoid memory leak

--- solara/server/patch.py
+++ solara/server/patch.py
@@ -259,18 +259,23 @@ def WidgetContextAwareThread__init__(self, *args, **kwargs):
 
 def Thread_debug_run(self):
     if not hasattr(self, "current_context"):
+        # this happens when a thread was running before we patched
         return Thread__run(self)
     if self.current_context:
         kernel_context.set_context_for_thread(self.current_context, self)
-        shell = self.current_context.kernel.shell
-        shell.display_pub.register_hook(shell.display_in_reacton_hook)
+        display_pub = self.current_context.kernel.shell.display_pub
+        display_in_reacton_hook = self.current_context.kernel.shell.display_in_reacton_hook
+        display_pub.register_hook(display_in_reacton_hook)
     try:
         context = self.current_context or solara.util.nullcontext()
         with pdb_guard(), context:
             Thread__run(self)
     finally:
-        if self.current_context:
-            shell.display_pub.unregister_hook(shell.display_in_reacton_hook)
+        current_context = self.current_context
+        self.current_context = None
+        kernel_context.clear_context_for_thread(self)
+        if current_context:
+            display_pub.unregister_hook(display_in_reacton_hook)
 
 
 _patched = False
